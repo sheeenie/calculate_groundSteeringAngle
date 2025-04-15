daa5357504bb463a7c9fe1947a4e5e8dd1821d3a
Ling Svahn <guslingsv@student.gu.se>
2025-04-15 16:56:41 +0200

    #9 Remove tests for helloworld.cpp

--- test/Testhelloworld.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this once per test-runner!
-
-#include "catch.hpp"
-#include "helloworld.hpp"
-
-TEST_CASE("Test Hello World.") {
-    REQUIRE(argv == 2);
-}
-
