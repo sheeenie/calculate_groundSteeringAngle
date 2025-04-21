b96bbcb0d5c489c90a27dc7056231f39ab1196e2
Sheenie <sheenie@student.chalmers.se>
2025-04-22 04:22:39 +0900

    #11 Fix hsv script to run separate terminals for the 3 microservices

--- scripts/run-hsv-stack.sh
+++ scripts/run-hsv-stack.sh
@@ -1,28 +1,30 @@
-# local testing
 
 #!/bin/bash
 
-# Run OpenDLV vehicle view
-echo "Starting openDLV-vehicle-view..."
-docker run --rm --init --net=host --name=opendlv-vehicle-view \
-  -v "$PWD":/opt/vehicle-view/recordings \
+# Start OpenDLV Vehicle View
+gnome-terminal -- bash -c "cd '$PWD'; echo '[OpenDLV] Starting...'; docker run --rm --init --net=host --name=opendlv-vehicle-view \
+  -v '$PWD':/opt/vehicle-view/recordings \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -p 8081:8081 chrberger/opendlv-vehicle-view:v0.0.64 &
+  -p 8081:8081 chrberger/opendlv-vehicle-view:v0.0.64; exec bash"
 
-sleep 3
+sleep 2
 
-# Run h264decoder
-echo "Starting h264decoder..."
-docker run --rm -ti --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp:/tmp h264decoder:v0.0.5 --cid=253 --name=img &
+# Start h264decoder (producer)
+gnome-terminal -- bash -c "cd '$PWD'; echo '[h264decoder] Starting...'; docker run --rm --net=host --ipc=host \
+  -e DISPLAY=\$DISPLAY -v /tmp:/tmp h264decoder:v0.0.5 \
+  --cid=253 --name=img; exec bash"
 
-sleep 3
+# Wait until shared memory file is available
+echo "Waiting for /tmp/img shared memory to be created by h264decoder..."
+while [ ! -e /tmp/img ]; do
+    sleep 1
+done
+echo "Shared memory found!"
 
-# Run HSV inspector
-echo "Starting hsv-filter microservice..."
-docker run --rm -ti --init --ipc=host -v /tmp:/tmp -e DISPLAY=$DISPLAY hsv:latest \
+sleep 1
+
+# Start HSV filter (consumer)
+gnome-terminal -- bash -c "cd '$PWD'; echo '[HSV filter] Starting...'; docker run --rm --net=host --ipc=host \
+  -v /tmp:/tmp -e DISPLAY=\$DISPLAY hsv:latest \
   --name=img --width=640 --height=480 \
-  --hue-min=100 --hue-max=140 \
-  --sat-min=150 --sat-max=255 \
-  --val-min=50 --val-max=255
-
-# Done
+  --hue-min=100 --hue-max=140 --sat-min=150 --sat-max=255 --val-min=50 --val-max=255; exec bash"
