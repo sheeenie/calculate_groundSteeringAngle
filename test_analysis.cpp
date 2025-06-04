00618d207a08b1020847994d7bfd5fac5a667cb8
Samuel Partain <samuel.partain@gmail.com>
2025-06-04 15:32:54 +0200

    #21 change included headers

--- test_analysis.cpp
+++ test_analysis.cpp
@@ -1,5 +1,5 @@
-#include "./microservices/steering-angle-microservice/src/cluon-complete-v0.0.127.hpp"
-#include "./microservices/steering-angle-microservice/src/opendlv-standard-message-set-v0.9.6.odvd"
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
 #include <cmath>
 #include <fstream>
 #include <iostream>
