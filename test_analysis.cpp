084cb35f3c8fba10c2b357b9861ed3de7e06e4ab
Samuel Partain <samuel.partain@gmail.com>
2025-06-04 14:48:21 +0200

    #21 Add test infrastructure & add first attempt at running test

---
+++ test_analysis.cpp
@@ -0,0 +1,9 @@
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+
